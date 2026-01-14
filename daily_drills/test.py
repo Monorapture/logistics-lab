0675a20ead991d8d9b3dcdf191a5ca58cfd77340
Kilian Sender <248671130+Monorapture@users.noreply.github.com>
2026-01-14 19:37:47 +0100

    instead of time.sleep(2) i used sleep(2) by importing the sleep function from the time module

--- daily_drills/test.py
+++ daily_drills/test.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep as sleep
 
 def start_schicht():
     print("--- WILLKOMMEN IM LAGER ---")
@@ -9,10 +9,10 @@ def start_schicht():
     print("C: Gabelstapler fahren mit Schein")
     print("D: Gabelstapler fahren ohne Schein")
     
-    # Hier wartet der Computer auf deine Eingabe
+    
     wahl = input("Tippe A, B, C oder D: ")
     
-    # Wir machen die Eingabe klein (.lower()), damit "a" und "A" funktionieren
+    
     if wahl.lower() == "a":
         lkw_abladen()
     elif wahl.lower() == "b":
@@ -23,53 +23,53 @@ def start_schicht():
         gabelstapler_fahren_ohne_schein()
     else:
         print("Das habe ich nicht verstanden. Du stolperst über eine Palette.")
-        start_schicht() # Startet von vorne (Rekursion - Vorsicht, aber cool!)
+        start_schicht() 
 
 def gabelstapler_fahren_mit_schein():
     print("\nDu fährst den Gabelstapler...")
-    time.sleep(2)
+    sleep(2)
     print("Du bringst die Palette zum LKW.")
-    time.sleep(2)
+    sleep(2)
     print("Du hast die Palette sicher zum LKW gebracht.")
-    time.sleep(2)
+    sleep(2)
     print("Jochen ist glücklich und ihr geht in den Keller saufen.")
 
 def gabelstapler_fahren_ohne_schein():
     print("\nDu fährst den Gabelstapler ohne Schein...")
-    time.sleep(2)
+    sleep(2)
     print("Ein Kollege hält dich an und verlangt deinen Schein.")
-    time.sleep(2)
+    sleep(2)
     print("Du fährst den Gabelstapler ohne Schein...")
-    time.sleep(2)
+    sleep(2)
     print("GAME OVER - Die Berufsgenossenschaft ist sauer.")
 
 def lkw_abladen():
     print("\nDu öffnest den LKW...")
-    time.sleep(2)
+    sleep(2)
     print("Oh nein! Die Ladung ist ungesichert!")
-    time.sleep(2)
+    sleep(2)
     print("Alles fällt dir entgegen.")
-    time.sleep(2)
+    sleep(2)
     print("VERLUST - Die Berufsgenossenschaft ist sauer.")
 
 def chef_gespraech():
     print("\nDer Chef spricht mit dir...")
-    time.sleep(2)
+    sleep(2)
     print("Der Chef ist gut drauf.")
-    time.sleep(2)
+    sleep(2)
     print("Der Chef sagt scherzhaft: 'Langsam glaube ich du säufst mehr als du verdienst!'")
     
     
 
 def kaffee_trinken():
     print("\nDer Kaffee schmeckt super.")
-    time.sleep(2)
+    sleep(2)
     print("Du bist wach und motiviert.")
-    time.sleep(2)
+    sleep(2)
     print("Plötzlich ruft der Chef!")
-    time.sleep(2)
+    sleep(2)
     chef_gespraech()
-    time.sleep(2)
+    sleep(2)
     print("GEWINNEN - Du hast den Tag überlebt.")
     
 
